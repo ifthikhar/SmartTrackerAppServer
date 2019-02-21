3e2bf4ff57693db905ac2f9c474866e0ba34289d
ifthikhar <ifthikharmd@gmail.com>
2019-02-21 08:43:22 -0600

    Changing the Tomcat Server for deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {Router, RouterModule, Routes} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { NotesComponent } from './notes/notes.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NoteComponent } from './notes/note/note.component';
+import { NotetextfilterPipe } from './shared/notetextfilter.pipe';
 
+
+const appRoutes: Routes = [
+  {
+    path: 'notes',
+    component: NotesComponent
+  },
+  {
+    path: 'feedback',
+    component: FeedbackComponent
+  },
+  {
+    path: '',
+    component: NotesComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FeedbackComponent,
+    NotesComponent,
+    PageNotFoundComponent,
+    NoteComponent,
+    NotetextfilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/notetextfilter.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { NotetextfilterPipe } from './notetextfilter.pipe';
+
+describe('NotetextfilterPipe', () => {
+  it('create an instance', () => {
+    const pipe = new NotetextfilterPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
